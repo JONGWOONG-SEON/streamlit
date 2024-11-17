887d656b68f09f8651422e1b2f9e90c2d31c1d01
face <www11288@naver.com>
2024-11-18 00:31:45 +0900

    add packages

--- driver/connector.py
+++ driver/connector.py
@@ -3,27 +3,27 @@ import streamlit as st
 from driver.preprocessor import preprocess
 from sqlalchemy import engine
 
-# class call_db:
-#     def __init__(self, database ,host, dbname, id, pwd, port):
-#         self.fetch = None
-#         if database == 'Postgresql':
-#             self.connection = psycopg2.connection(host=host
-#                                               , dbname=dbname
-#                                               , user=id
-#                                               , password=pwd
-#                                               , port=port)
-#             self.cursor = self.connection.cursor()
-#             st.session_state['database_connection'] = True
-#
-#     def __call__(self,sql):
-#         self.cursor.execute(sql)
-#         self.fetch = self.cursor.fetchall()
-#         return self.fetch
-#
-#     def is_connect(self):
-#         # return st.success("DB connect"),st.rerun()
-#         return st.success("DB connection")
-
 class call_db:
-    def __init__(self):
-        engine.Connection()
+    def __init__(self, database ,host, dbname, id, pwd, port):
+        self.fetch = None
+        if database == 'Postgresql':
+            self.connection = psycopg2.connection(host=host
+                                              , dbname=dbname
+                                              , user=id
+                                              , password=pwd
+                                              , port=port)
+            self.cursor = self.connection.cursor()
+            st.session_state['database_connection'] = True
+
+    def __call__(self,sql):
+        self.cursor.execute(sql)
+        self.fetch = self.cursor.fetchall()
+        return self.fetch
+
+    def is_connect(self):
+        # return st.success("DB connect"),st.rerun()
+        return st.success("DB connection")
+
+# class call_db:
+#     def __init__(self):
+#         engine.Connection()
